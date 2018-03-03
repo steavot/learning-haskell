ebf5dc7818bb2cb314513c8872a0b6b2ed333241
steavot <tomsteavenson@gmail.com>
2018-03-03 14:29:56 +0000

    use fmap over the IO action

--- parser/mdtoHTML.hs
+++ parser/mdtoHTML.hs
@@ -2,6 +2,6 @@ import System.IO
 import MdHTMLParse
 
 main = do
-    contents <- readFile "test_input.md"
-    writeFile "test_output.html" (writeHTMLBlock . readMdBlock $ contents)
+  contents <- fmap readMdBlock . readFile $ "test_input.md"
+  writeFile "test_output.html" . writeHTMLBlock $ contents
 
