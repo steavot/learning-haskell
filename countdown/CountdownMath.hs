b24cfa3725e940bed4def5d8d9c0f3d3036dbd9c
Richard Lupton <richard.lupton@gmail.com>
2018-04-08 13:38:51 +0100

    First review markups

--- countdown/CountdownMath.hs
+++ countdown/CountdownMath.hs
@@ -17,6 +17,9 @@ module CountdownMath
 import Data.List
 import GHC.Exts
 
+
+-- RL You don't need ln and sum - you only use binary operations which makes RPNs a little
+-- more predictable and easier encode (I know this is just a verbatim copy)
 -- below is the RPN evaluator from Learn You A Haskell
 solveRPN :: String -> Float
 solveRPN = head . foldl foldingFunction [] . words
@@ -29,6 +32,21 @@ solveRPN = head . foldl foldingFunction [] . words
           foldingFunction xs "sum" = [sum xs]
           foldingFunction xs numberString = read numberString:xs
 
+
+-- RL Do you want to represent an RPN as a string? Presumably this
+--    makes it easier to generate expressions. I wonder if there are clever
+--    ways of efficiently generating RPN patterns which are "valid". Here is
+--    probably where I would start:
+--
+-- newtype RPN = RPN [String]  (or type RPN = [String] if you prefer)
+--
+-- isOp :: String -> Bool
+-- decodeOp :: String -> (Float -> Float -> Maybe Float)
+-- eval :: RPN -> Float
+-- rank :: RPN -> Int    -- height of stack after evaluation - note you don't need
+                         -- to know about the numbers at all, only that they are numbers
+                         -- you want to filter low rank (<1) RPNs
+
 -- For our purposes we want:
 --   Just the 1st 4 operators
 --   Evaluation to fail if the value on the top of the stack becomes either negative or a fraction
@@ -40,26 +58,21 @@ solveRPN = head . foldl foldingFunction [] . words
 --   Division by zero (this is covered by not allowing subtraction of equal numbers)
 --   Division resulting in a fraction
 --   Subtraction resulting in a negative
+-- RL decodeOp above will let you decouple the operation logic from the aggregation
 countdownRPN :: String -> Maybe Int
 countdownRPN = fmap head . (foldl foldingFunction (Just [])) . words
     where foldingFunction :: Maybe [Int] -> String -> Maybe [Int]
           -- Accumulator is Nothing - the calculation has already failed
+          -- RL See Control.Monad's foldM
           foldingFunction Nothing _ = Nothing
-          -- Operator cannot be the 1st or 2nd element of an RPN string
-          foldingFunction (Just []) "*" = Nothing
-          foldingFunction (Just []) "+" = Nothing
-          foldingFunction (Just []) "-" = Nothing
-          foldingFunction (Just []) "/" = Nothing
-          foldingFunction (Just x:[]) "*" = Nothing
-          foldingFunction (Just x:[]) "+" = Nothing
-          foldingFunction (Just x:[]) "-" = Nothing
-          foldingFunction (Just x:[]) "/" = Nothing
           -- Dealing with valid RPN strings from here
           foldingFunction (Just (x:y:ys)) "*" = Just ((x * y):ys)
           foldingFunction (Just (x:y:ys)) "+" = Just ((x + y):ys)
           foldingFunction (Just (x:y:ys)) "-" = if y > x then Just ((y - x):ys) else Nothing
           foldingFunction (Just (x:y:ys)) "/" = if y `mod` x == 0 then Just ((y `div` x):ys) else Nothing
-          foldingFunction (Just xs) numberString = Just ((read numberString :: Int):xs)
+          foldingFunction (Just xs) string
+            | string `elem` ["+", "-", "*", "/"] = Nothing
+            | otherwise = Just ((read string :: Int):xs)    -- RL make read use Maybe
 
 -- Countdown maths problem doesn't require all the numbers are used.  The minimum we can use is 2.
 -- We need to also consider all the combinations of not all the tiles being used.
@@ -67,13 +80,16 @@ allRPNs :: [String] -> [String]
 allRPNs tiles = nub . concat . fmap rpnPermutations . tileCombines $ tiles
   where tileCombines tiles = sortWith length [t | t <- subsequences tiles, length t > 1]
 
+-- RL You calculate operatorPerms and then essentially do it again in combineOpsAndTiles
+-- Can that be avoided?
 rpnPermutations :: [String] -> [String]
 rpnPermutations tiles = concat (fmap (combineOpsAndTiles tiles) (operatorPerms . length $ tiles))
 
 combineOpsAndTiles :: [String] -> String -> [String]
 combineOpsAndTiles tiles ops = fmap unwords . permutations $ (tiles ++ (words ops))
 
--- operatorPerms. Given a number we want to produce every combination of +,-,*,/ that is that number long minus 1.
+-- RL operatorPerms = mapM (const operators) (replicate (x-1) 0)  note the 0 has no relevance here, we just need a list of length x-1 to map over
+-- operatorPerms. Given a number we want to produce every combination of +,-,*,/ that is that number long minus 1. -- RL subtracting 1 here leads to a slightly misleading API
 -- As we know this number will be between 2 and 6, we can be lazy
 operatorPerms :: Int -> [String]
 operatorPerms x
